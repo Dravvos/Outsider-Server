4fae3db8515c7a96ca727f49e824323ef636ce61
Dravvos <daniddias53@gmail.com>
2025-03-16 11:22:26 -0300

    Alter folder name

--- Outsider.PedidoAPI/obj/Debug/net8.0/Outsider.PedidoAPI.AssemblyInfo.cs
+++ Outsider.PedidoAPI/obj/Debug/net8.0/Outsider.PedidoAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Outsider.PedidoAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaa6eaaea2c65d09d2e9d739b11e6508ee51e94e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+610f192f7783e76b47f7037c9497d28de360867b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Outsider.PedidoAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Outsider.PedidoAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
