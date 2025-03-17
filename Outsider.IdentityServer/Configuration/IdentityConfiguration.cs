26e18700b6a20d15dd0098e724c39d16334b4150
Dravvos <daniddias53@gmail.com>
2025-03-16 23:16:06 -0300

    Allow offline access option to True

--- Outsider.IdentityServer/Configuration/IdentityConfiguration.cs
+++ Outsider.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -48,6 +48,7 @@ namespace Outsider.IdentityServer.Configuration
                         "Outsider",
                     },
                     RequireClientSecret=false,
+                    AllowOfflineAccess=true,
                 }
             };
     }
