9ca2292018eb87300b7d6c8745098d38666bbc24
Dravvos <daniddias53@gmail.com>
2025-03-17 22:50:13 -0300

    Tabela Geral Swagger

--- Outsider.TabelaGeralAPI/Program.cs
+++ Outsider.TabelaGeralAPI/Program.cs
@@ -104,11 +104,12 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
     
     app.UseCors(cors =>
     {
