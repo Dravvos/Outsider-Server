d8937ef0fdb2bb68658054a4aa2f6be7331f81d0
Dravvos <daniddias53@gmail.com>
2025-09-19 17:48:36 -0300

    Ajuste CarrinhoAPI

--- Outsider.IdentityServer/obj/Debug/net8.0/Outsider.IdentityServer.AssemblyInfo.cs
+++ Outsider.IdentityServer/obj/Debug/net8.0/Outsider.IdentityServer.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Outsider.IdentityServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95a409a94301d9fbbe65db8ad59337df0f879851")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a92e00a2c5805237630f3d6df92ed2c843b4dd3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Outsider.IdentityServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Outsider.IdentityServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
